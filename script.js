a161df716c243a8752e1a3c6f61ea902da08b493
Mahrozfatima786 <mahrozfatima156@gmail.com>
2025-06-28 12:49:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	return str.split('').filter(function (character, index, obj) {
+        return obj.indexOf(character) === obj.lastIndexOf(character);
+    }).shift();
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
